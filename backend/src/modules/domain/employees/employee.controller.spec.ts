22174611b6415a39d0d49b63aec46a467fe70663
Lou <PrimusPilus@users.noreply.github.com>
2021-03-31 16:26:51 -0700

    Update employee.controller.spec.ts

--- backend/src/modules/domain/employees/employee.controller.spec.ts
+++ backend/src/modules/domain/employees/employee.controller.spec.ts
@@ -64,7 +64,7 @@ describe('Employee Controller', () => {
       // Arrange
       const request = {
         employeeId: 100,
-        XXirst Name: 'Jimmy',
+        xName: 'Jimmy',
         lastName: 'Ramos',
         middleName: 'Josue',
         secondLastName: 'Banegas',
